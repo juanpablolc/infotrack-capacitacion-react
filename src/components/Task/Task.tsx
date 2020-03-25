4b8063940318961c4b8b399cbbf0af7be5d54be0
Juan Pablo López Cifuentes <juan.lopez@infotrack.com.co>
2020-03-25 15:18:00 -0500

    State in class-based and functional components

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 
 interface TaskProps {
     message: string;
 }
 
+// COMPONENTE DE CLASE / CLASS-BASED COMPONENT
+
+/*
+class Task extends Component<TaskProps> {
+    state = {
+        completed: false
+        // color: '#FFF'
+    };
+
+    handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const stateCopy = { ...this.state };
+        stateCopy.completed = !stateCopy.completed;
+        this.setState(stateCopy);
+    };
+
+    render() {
+        return (
+            <div>
+                {this.props.message}
+                <input checked={this.state.completed} onChange={this.handleChange} type="checkbox"></input>
+            </div>
+        );
+    }
+}
+*/
+
+// COMPONENTE FUNCIONAL / STATELESS COMPONENT
+
 const Task = (props: TaskProps) => {
-    return <div>{props.message}</div>;
+    const [completed, setCompleted] = useState<boolean>(false);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCompleted(!completed);
+    };
+
+    return (
+        <div>
+            {props.message}
+            <input checked={completed} onChange={handleChange} type="checkbox"></input>
+        </div>
+    );
 };
 
 export default Task;
